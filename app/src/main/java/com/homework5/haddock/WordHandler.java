f7de61a962b5f463fa872bf205adbcc76cae837f
Joakim Markén <jmarken@kth.se>
2017-12-08 13:47:10 +0100

    Stabil Push

---
+++ app/src/main/java/com/homework5/haddock/WordHandler.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
+class WordHandler {
+    private String[] words;
+
+
+    public WordHandler() throws IOException {
+        createList();
+    }
+
+    private void createList() throws IOException {
+        FileReader fileReader = new FileReader(new java.io.File("words.txt"));
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String line;
+        ArrayList<String> tempList = new ArrayList<>();
+        while ((line = bufferedReader.readLine()) != null) {
+            tempList.add(line);
+        }
+        words = tempList.toArray(new String[0]);
+        fileReader.close();
+    }
+
+    public String randomWord() {
+        int randomNum = ThreadLocalRandom.current().nextInt(0, words.length);
+        return "\"" + words[randomNum] + "\"";
+    }
+
+    public void printFullList(){
+        for(int i = 0; i < words.length; i++){
+            System.out.print("\"");
+            System.out.print(words[i]);
+            System.out.println("\"");
+        }
+
+    }
+}

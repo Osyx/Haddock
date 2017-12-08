2c7516b057dde048e75bfeb862aaceea3636c6a1
Oscar Falkman <ofalkman@gmail.com>
2017-12-08 14:18:46 +0100

    Internet stuff that noone understands...

--- app/src/main/java/com/homework5/haddock/MainActivity.java
+++ app/src/main/java/com/homework5/haddock/MainActivity.java
@@ -1,7 +1,8 @@
 package com.homework5.haddock;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void fetchSwearing(View view) {
+
+    }
+
+
+
 }
+

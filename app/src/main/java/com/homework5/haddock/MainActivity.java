ce14a4d529878fb4fed61be4889f38b40de69de9
Oscar Falkman <ofalkman@gmail.com>
2017-12-09 03:21:18 +0100

    Ready enough for the big task of fetching from server and DB.

--- app/src/main/java/com/homework5/haddock/MainActivity.java
+++ app/src/main/java/com/homework5/haddock/MainActivity.java
@@ -1,9 +1,10 @@
 package com.homework5.haddock;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toolbar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void fetchSwearing(View view) {
+        String newCitation = "Bomber och granater, du tryckte på knappen...\nHär tror du att du ska få snack och strunt och snack, nej nu går skam över torra land, skäms landkrabba!";
+        changeCitation(view, newCitation);
+
+    }
+
+    public void changeCitation(View view, String newMessage) {
+        TextView textView = findViewById(R.id.citationBox);
+        CharSequence initialShout = view.getContext().getText(R.string.initial_shout);
+        if(textView.getText().equals(initialShout))
+            textView.setText(newMessage);
+        else
+            textView.setText(initialShout);
 
     }
 

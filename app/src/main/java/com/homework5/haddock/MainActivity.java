a1e0e467becae183f5574fa26729b3778b187792
Oscar Falkman <ofalkman@gmail.com>
2017-12-11 15:30:55 +0100

    Fetch from internet or local.

--- app/src/main/java/com/homework5/haddock/MainActivity.java
+++ app/src/main/java/com/homework5/haddock/MainActivity.java
@@ -1,14 +1,24 @@
 package com.homework5.haddock;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private String URL = "http://osyx.azurewebsites.net/haddock/words.txt";
+    private String WORD_ERROR = "Something went wrong during word generation.";
+    private String NO_NETWORK_MSG = "No network detected, fetching locally...";
+    boolean toast = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,22 +31,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void changeCitation(String newMessage) {
+        if(toast) {
+            Toast.makeText(getApplicationContext(), NO_NETWORK_MSG, Toast.LENGTH_LONG).show();
+            toast = false;
+        }
         TextView textView = findViewById(R.id.citationBox);
         textView.setText(newMessage);
+        while(true)
+            Log.wtf(TAG, "looping");
 
     }
 
     private class FetchCitation extends AsyncTask<Void, Void, String> {
 
-
         @Override
         protected String doInBackground(Void... voids) {
             String citation;
             try {
-                WordHandler wh = new WordHandler(getResources().openRawResource(R.raw.words));
+                ConnectivityManager connMgr = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connMgr != null ? connMgr.getActiveNetworkInfo() : null;
+                WordHandler wh;
+                if (networkInfo != null && networkInfo.isConnected()) {
+                    wh = new WordHandler(new NetworkHandler().getPageContent(URL));
+                } else {
+                    wh = new WordHandler(getResources().openRawResource(R.raw.words));
+                    toast = true;
+                }
                 citation = wh.randomWord();
             } catch (IOException e) {
                 citation = "Nä nu blommar asfalten och skam går på torra land, det blev något knas på skutan...";
+                Log.e(TAG, WORD_ERROR);
             }
             return citation;
         }

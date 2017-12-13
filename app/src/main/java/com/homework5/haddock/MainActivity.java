bbd1bfb2511f1ffc7e8f229d44b425a17152f2ab
Oscar Falkman <ofalkman@gmail.com>
2017-12-13 21:17:25 +0100

    Internet connectivity using sockets.

--- app/src/main/java/com/homework5/haddock/MainActivity.java
+++ app/src/main/java/com/homework5/haddock/MainActivity.java
@@ -11,19 +11,26 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.homework5.haddock.network.DownloadCallback;
+import com.homework5.haddock.network.NetworkFragment;
+
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DownloadCallback {
     private static final String TAG = "MainActivity";
     private String URL = "http://osyx.azurewebsites.net/haddock/words.txt";
     private String WORD_ERROR = "Something went wrong during word generation.";
     private String NO_NETWORK_MSG = "No network detected, fetching locally...";
+    private NetworkFragment mNetworkFragment;
+    private boolean mDownloading = false;
     boolean toast = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mNetworkFragment = NetworkFragment.getInstance(getFragmentManager(), "https://www.google.com");
+
     }
 
     public void fetchSwearing(View view) {
@@ -39,6 +46,59 @@ public class MainActivity extends AppCompatActivity {
         textView.setText(newMessage);
     }
 
+    private void startDownload() {
+        if (!mDownloading && mNetworkFragment != null) {
+            // Execute the async download.
+            mNetworkFragment.startDownload();
+            mDownloading = true;
+        }
+    }
+
+
+    @Override
+    public void updateFromDownload(Object result) {
+        // Update your UI here based on result of download.
+    }
+
+    @Override
+    public NetworkInfo getActiveNetworkInfo() {
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo;
+    }
+
+    @Override
+    public void onProgressUpdate(int progressCode, int percentComplete) {
+        switch(progressCode) {
+            // You can add UI behavior for progress updates here.
+            case Progress.ERROR:
+                Toast.makeText(getApplicationContext(), "Progress error", Toast.LENGTH_LONG).show();
+                break;
+            case Progress.CONNECT_SUCCESS:
+                Toast.makeText(getApplicationContext(), "Connection success", Toast.LENGTH_LONG).show();
+                break;
+            case Progress.GET_INPUT_STREAM_SUCCESS:
+                Toast.makeText(getApplicationContext(), "Got input stream", Toast.LENGTH_LONG).show();
+                break;
+            case Progress.PROCESS_INPUT_STREAM_IN_PROGRESS:
+                Toast.makeText(getApplicationContext(), "Reading from input stream", Toast.LENGTH_LONG).show();
+                break;
+            case Progress.PROCESS_INPUT_STREAM_SUCCESS:
+                Toast.makeText(getApplicationContext(), "Read from input stream successfully", Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
+
+    @Override
+    public void finishDownloading() {
+        mDownloading = false;
+        if (mNetworkFragment != null) {
+            mNetworkFragment.cancelDownload();
+        }
+    }
+
+
     private class FetchCitation extends AsyncTask<Void, Void, String> {
 
         @Override
@@ -47,15 +107,8 @@ public class MainActivity extends AppCompatActivity {
             try {
                 ConnectivityManager connMgr = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
                 NetworkInfo networkInfo = connMgr != null ? connMgr.getActiveNetworkInfo() : null;
-                WordHandler wh;
-                if (networkInfo != null && networkInfo.isConnected()) {
-                    wh = new WordHandler(new NetworkHandler().getPageContent(URL));
-                } else {
-                    wh = new WordHandler(getResources().openRawResource(R.raw.words));
-                    toast = true;
-                }
-                citation = wh.randomWord();
-            } catch (IOException e) {
+                citation = "************************************* WORD FETCHED FROM SERVER **************************************";
+            } catch (Exception e) {
                 citation = "Nä nu blommar asfalten och skam går på torra land, det blev något knas på skutan...";
                 Log.e(TAG, WORD_ERROR);
             }
